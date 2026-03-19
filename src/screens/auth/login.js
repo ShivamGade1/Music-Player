903f62bce7f016169c5143b7c2e02f9b1f6ae20e
Shivam Gade <shivamgade6@gmail.com>
2026-03-19 14:11:36 +0530

    fix build error

--- src/screens/auth/login.js
+++ src/screens/auth/login.js
@@ -1,21 +1,18 @@
 import React from "react";
-import { getLoginUrl } from "../../spotify";
+import { loginEndpoint } from "../../spotify";
+import "./login.css";
 
-function Login() {
-  const handleLogin = async () => {
-    const url = await getLoginUrl();
-    window.location.href = url;
-  };
-
+export default function Login() {
   return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Music Player 🎵</h1>
-
-      <button onClick={handleLogin}>
-        Login with Spotify
-      </button>
+    <div className="login-page">
+      <img
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"
+        alt="logo-spotify"
+        className="logo"
+      />
+      <a href={loginEndpoint}>
+        <div className="login-btn">LOG IN</div>
+      </a>
     </div>
   );
 }
-
-export default Login;
