7bb09c29441e1c102f6693514ef717f396cf1bbc
Shivam Gade <shivamgade6@gmail.com>
2026-03-20 00:16:27 +0530

    fix

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,66 +1,74 @@
 import React, { useState, useEffect } from "react";
 import "./sidebar.css";
 import SidebarButton from "./sidebarButton";
-import { MdFavorite } from "react-icons/md";
+import { MdFavorite, MdSpaceDashboard } from "react-icons/md";
 import { FaGripfire, FaPlay, FaSignOutAlt } from "react-icons/fa";
 import { IoLibrary } from "react-icons/io5";
-import { MdSpaceDashboard } from "react-icons/md";
 
 export default function Sidebar() {
   const [image, setImage] = useState(
-  "https://i.imgur.com/6VBx3io.png"
-);
+    "https://i.imgur.com/6VBx3io.png"
+  );
 
   useEffect(() => {
-  const token = localStorage.getItem("access_token");
+    const token = localStorage.getItem("access_token");
 
-  console.log("TOKEN USED:", token);
+    console.log("TOKEN USED:", token);
 
-  if (!token) {
-    console.error("NO TOKEN FOUND");
-    return;
-  }
+    if (!token) {
+      console.error("NO TOKEN FOUND");
+      return;
+    }
 
-  fetch("https://api.spotify.com/v1/me", {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
-    .then((res) => {
-      console.log("STATUS:", res.status);
-
-      if (!res.ok) {
-        return res.text().then((text) => {
-          console.error("API ERROR:", text);
-        });
-      }
-
-      return res.json();
+    fetch("https://api.spotify.com/v1/me", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     })
-    .then((data) => {
-      console.log("USER DATA:", data);
+      .then((res) => {
+        console.log("STATUS:", res.status);
 
-      if (data && data.images && data.images.length > 0) {
-        setImage(data.images[0].url);
-      }
-    })
-    .catch((err) => console.error("Fetch error:", err));
-}, []);
+        if (!res.ok) {
+          return res.text().then((text) => {
+            console.error("API ERROR:", text);
+          });
+        }
+
+        return res.json();
+      })
+      .then((data) => {
+        console.log("USER DATA:", data);
+
+        if (data && data.images && data.images.length > 0) {
+          setImage(data.images[0].url);
+        }
+      })
+      .catch((err) => console.error("Fetch error:", err));
+  }, []);
 
   return (
     <div className="sidebar-container">
       <img src={image} className="profile-img" alt="profile" />
 
-      <div>
-        <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
-        <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
-        <SidebarButton title="Player" to="/player" icon={<FaPlay />} />
-        <SidebarButton title="Favorite" to="/favorite" icon={<MdFavorite />} />
-        <SidebarButton title="Library" to="/" icon={<IoLibrary />} />
+      <div className="sidebar-buttons">
+      <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
+      <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
+      <SidebarButton title="Player" to="/player" icon={<FaPlay />} />
+      <SidebarButton title="Favorite" to="/favorite" icon={<MdFavorite />} />
+      <SidebarButton title="Library" to="/" icon={<IoLibrary />} />
       </div>
 
-      <SidebarButton title="Sign Out" to="/" icon={<FaSignOutAlt />} />
+      <div
+        className="btn-body"
+        onClick={() => {
+          localStorage.removeItem("access_token");
+          window.location.reload();
+        }}
+      >
+        <FaSignOutAlt />
+        <p className="btn-title">Sign Out</p>
+        </div>
     </div>
   );
 }
