d560fb00be2fe585e58853d546571c52dc8b7541
Shivam Gade <shivamgade6@gmail.com>
2026-03-24 17:50:42 +0530

    color change

--- src/screens/library.js
+++ src/screens/library.js
@@ -1,17 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import APIKIT from "../spotify";
 
 export default function Library() {
+  /*const [playlists, setPlaylists] = useState([]);
 
   useEffect(() => {
-    console.log("LIBRARY LOADED"); // debug
+    console.log("LIBRARY LOADED");
 
-    APIKIT.get("/me/playlists") // ✅ IMPORTANT "/" added
+    APIKIT.get("/me/playlists")
       .then((response) => {
         console.log("PLAYLIST DATA:", response.data);
+
+        // ✅ IMPORTANT LINE
+        setPlaylists(response.data.items);
       })
       .catch((err) => console.error("ERROR:", err));
-  }, []);
+  }, []);*/
 
-  return <div className="screen-container"></div>;
+  return (
+    /*<div className="screen-container">
+      {playlists.map((playlist) => (
+        <div key={playlist.id}>
+          {playlist.name}
+        </div>
+      ))}
+    </div>*/
+    <div className="screen-container">
+      Library
+    </div>
+  );
 }
